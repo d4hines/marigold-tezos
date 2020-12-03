c849f6868a9d4287098ce6d8f5e2bbb291e23975
Gabriel Alfour <gabriel.alfour@gmail.com>
2020-12-03 14:08:22 +0100

    fix bug: missing encoding

--- src/proto_alpha/lib_protocol/michelson_v1_primitives.ml
+++ src/proto_alpha/lib_protocol/michelson_v1_primitives.ml
@@ -1117,7 +1117,10 @@ let prim_encoding =
          ("READ_TICKET", I_READ_TICKET);
          ("SPLIT_TICKET", I_SPLIT_TICKET);
          ("JOIN_TICKETS", I_JOIN_TICKETS);
-         ("GET_AND_UPDATE", I_GET_AND_UPDATE)
+         ("GET_AND_UPDATE", I_GET_AND_UPDATE);
+         (* /!\ NEW INSTRUCTIONS MUST BE ADDED AT THE END OF THE STRING_ENUM, FOR BACKWARD COMPATIBILITY OF THE ENCODING. *)
+         (* Alpha_009 addition *)
+         ("LOG", I_LOG);
          (* New instructions must be added here, for backward compatibility of the encoding. *)
          (* Keep the comment above at the end of the list *)
         ]
