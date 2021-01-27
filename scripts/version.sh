10bbb49ab9f5f74a4a0b24ad608759c204b08e59
jaiyalas <jaiyalas@gmail.com>
2021-01-27 23:49:50 +0800

    temporary fix dependency

--- scripts/version.sh
+++ scripts/version.sh
@@ -10,11 +10,11 @@ recommended_rust_version=1.44.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=5491aa2960fd7b103b4461772b7badb475061d70
+full_opam_repository_tag=66b57dd345f3bf176c18295e645d1b5f6f5891aa
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=dd4bc58ba8b5ba8d1fe24796623f51fee6e7a5c3
-opam_repository_url=https://gitlab.com/tezos/opam-repository.git
+opam_repository_tag=66b57dd345f3bf176c18295e645d1b5f6f5891aa
+opam_repository_url=https://gitlab.com/marigold/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
 ## Other variables, used both in Makefile and scripts
