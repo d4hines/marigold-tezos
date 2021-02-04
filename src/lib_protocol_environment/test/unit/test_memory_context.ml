0b79d6941575c3e592df42b2e63821cc3f1a9bf6
Manas <2601505-prometheansacrifice@users.noreply.gitlab.com>
2021-02-04 23:23:16 +0530

    Proto: resolve merge conflicts with master

--- src/lib_protocol_environment/test/unit/test_memory_context.ml
+++ src/lib_protocol_environment/test/unit/test_memory_context.ml
@@ -62,88 +62,6 @@ module Test_memory_context = struct
       ~msg:"foo is not a memeber anymore, after removing foo" ;
     return_unit
 
-  let copy ctxt ~from ~to_ =
-    Context.find_tree ctxt from
-    >>= function
-    | None ->
-        Lwt.return_none
-    | Some sub_tree ->
-        Context.add_tree ctxt to_ sub_tree >>= Lwt.return_some
-
-  let copy_dest_exists () =
-    let mc = Memory_context.empty in
-    let foo_value_1 = Bytes.of_string "foovalue-1" in
-    let foo_value_2 = Bytes.of_string "foovalue-2" in
-    Context.add mc ["foo"; "source"] foo_value_1
-    >>= fun mc ->
-    Context.add mc ["foo"; "dest"] foo_value_2
-    >>= fun mc ->
-    Context.find mc ["foo"; "source"]
-    >>= fun value_opt ->
-    Assert.equal_string_option (Some "foovalue-1") (c value_opt) ;
-    Context.find mc ["foo"; "dest"]
-    >>= fun value_opt ->
-    Assert.equal_string_option (Some "foovalue-2") (c value_opt) ;
-    copy mc ~from:["foo"; "source"] ~to_:["foo"; "dest"]
-    >>= (function
-          | Some mc ->
-              Context.find mc ["foo"; "source"]
-              >>= fun value_opt ->
-              Assert.equal_string_option (Some "foovalue-1") (c value_opt) ;
-              Lwt.return mc
-          | None ->
-              Assert.fail ".copy shouldn't have failed" "" "")
-    >>= fun mc ->
-    Context.find mc ["foo"; "dest"]
-    >>= fun value_opt ->
-    Assert.equal_string_option (Some "foovalue-1") (c value_opt) ;
-    return_unit
-
-  let copy_dest_non_existent () =
-    let mc = Memory_context.empty in
-    let foo_value_1 = Bytes.of_string "foovalue-1" in
-    Context.add mc ["foo"; "source"] foo_value_1
-    >>= fun mc ->
-    Context.find mc ["foo"; "non-existent-path"]
-    >>= function
-    | Some _ ->
-        Assert.fail_msg
-          "Setup invariant, non-existence key path should return None, failed"
-    | None ->
-        Lwt.return_unit
-        >>= fun () ->
-        copy mc ~from:["foo"; "source"] ~to_:["foo"; "non-existent-path"]
-        >>= (function
-              | Some mc ->
-                  Context.find mc ["foo"; "source"]
-                  >>= fun value_opt ->
-                  Assert.equal_string_option (Some "foovalue-1") (c value_opt) ;
-                  Lwt.return mc
-              | None ->
-                  Assert.fail ".copy shouldn't have failed" "" "")
-        >>= fun mc ->
-        Context.find mc ["foo"; "non-existent-path"]
-        >>= fun value_opt ->
-        Assert.equal_string_option (Some "foovalue-1") (c value_opt) ;
-        return_unit
-
-  let fold () =
-    let mc = Memory_context.empty in
-    let b = Bytes.of_string in
-    Context.add mc ["foo"; "baz"] (b "foovalue")
-    >>= fun mc ->
-    Context.add mc ["foo"; "bar"] (b "foobarvalue")
-    >>= fun mc ->
-    Context.fold mc ["foo"] ~init:[] ~f:(fun k v acc ->
-        match Context.Tree.kind v with
-        | `Tree ->
-            Lwt.return acc
-        | `Value _ ->
-            Lwt.return (k :: acc))
-    >>= fun folded_values ->
-    Assert.equal [["baz"]; ["bar"]] folded_values ;
-    return_unit
-
   let set_protocol () =
     let mc = Memory_context.empty in
     Context.set_protocol
@@ -185,18 +103,6 @@ let tests =
       "Memory_context.ml: basic operations (get, set, mem, dir_mem)"
       `Quick
       Test_memory_context.basic_ops;
-    Test.tztest
-      "Memory_context.ml: copy operation (when dest exists)"
-      `Quick
-      Test_memory_context.copy_dest_exists;
-    Test.tztest
-      "Memory_context.ml: copy operation (when dest does not exists)"
-      `Quick
-      Test_memory_context.copy_dest_non_existent;
-    Test.tztest
-      "Memory_context.ml: fold operation"
-      `Quick
-      Test_memory_context.fold;
     Test.tztest
       "Memory_context.ml: set_protocol operation"
       `Quick
